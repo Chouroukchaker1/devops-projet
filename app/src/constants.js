bd76cb7cf4731e08a2bf48d1a60042850f6f3168
Chouroukchaker1 <chourouk.chaker@polytechnicien.tn>
2025-01-11 18:25:04 +0100

    first

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:5000/api";
-export const URL_IMG = "http://localhost:5000/uploads/cars/";
+export const URL = "http://localhost:8899/api";
+export const URL_IMG = "http://localhost:8899/uploads/cars/";
 export const AUTH_TOKEN = localStorage.getItem("token");
 export const AUTH_USER = JSON.parse(localStorage.getItem("user"));
--- back/databases/sequelize.js
+++ back/databases/sequelize.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
+
 const sequelize = new Sequelize({
     database: "cars",
     username: "root",
-    password: "root",
-    host: "mysql",
+    password: "", // Assurez-vous que le mot de passe correspond bien à celui de votre configuration MySQL locale
+    host: "localhost", // Remplacez 'mysql' par 'localhost'
     dialect: "mysql",
-    port: 3306
+    port: 3306 // Assurez-vous que MySQL écoute bien sur ce port
 });
 
 export default sequelize;
